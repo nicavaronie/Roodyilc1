9956ac04c6a13dff770a5d8e6025c29050d3654c
Roody Altidort <raltidort@gmail.com>
2019-03-23 10:39:32 -0700

    commit initial

---
+++ app/src/main/java/com/example/cassandraseptenbre/roodyilc1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.cassandraseptenbre.roodyilc1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ChangeName(View view) {
+    TextView tvName = findViewById(R.id.tvName);
+    tvName.setText("Roody Altidort");
+    }
+
+}
